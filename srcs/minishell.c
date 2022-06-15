852fef1a44cafc74756504b2cdd5f3021c32279f
Glenn Ashli <glashli@student.21-school.ru>
2022-06-15 19:01:06 +0300

    Added signals

--- srcs/minishell.c
+++ srcs/minishell.c
@@ -1,10 +1,17 @@
 #include "minishell.h"
 
-int	main(void)
+int main(int argc, char **argv, char    **envp)
 {
-	char	prompt[12] = "minishell: ";
-	char	*in;
+	t_sigaction	action;
+	char		prompt[12] = "minishell: ";
+	char		*in;
 
+	(void)argc;
+	(void)argv;
+	(void)envp;
+	ft_initial_action(&action);
+	if (sigaction(SIGQUIT, &action, NULL) == -1
+		|| sigaction(SIGINT, &action, NULL) == -1)
 	in = readline(prompt);
 	while (in)
 	{
