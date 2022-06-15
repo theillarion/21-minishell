23c1b9ad1219eb4ae640ef33b74b7db5735df68e
Glenn Ashli <glashli@student.21-school.ru>
2022-06-15 19:01:21 +0300

    Added signals

--- includes/minishell.h
+++ includes/minishell.h
@@ -8,4 +8,14 @@
 
 # include "../lib/libft/libft.h"
 
+# include <unistd.h>
+# include <stdlib.h>
+# include <signal.h>
+
+typedef struct sigaction	t_sigaction;
+
+void	ft_initial_action(t_sigaction *action);
+
+int main(int argc, char **argv, char    **envp);
+
 #endif
