3b06181e55dd8f44e0d82a14d83976b0486b0ee5
Illarion <pro100illarion@gmail.com>
2022-06-21 21:14:34 +0300

    Added initial for variable

--- srcs/init.c
+++ srcs/init.c
@@ -2,8 +2,13 @@
 
 void	ft_init(t_environment	*env, const char	*name_shell)
 {
+	if (env == NULL)
+		return;
+	ft_initial_action(&env->action);
 	env->info.pwd = ft_get_pwd();
 	env->info.name_shell = ft_strdup(name_shell);
+	env->prompt.current_prompt = NULL;
 	env->prompt.is_need_change = false;
+	env->input_line = NULL;
 	ft_set_new_prompt(&env->prompt, env->info);
 }
