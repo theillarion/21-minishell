be9dcb87dd78a53125610ce5a5973f94486add01
Illarion <pro100illarion@gmail.com>
2022-06-21 20:59:01 +0300

    Added main struct and prototype functions

--- includes/minishell.h
+++ includes/minishell.h
@@ -12,15 +12,42 @@
 # include <stdlib.h>
 # include <signal.h>
 # include <stdbool.h>
+# include <errno.h>
 
 typedef struct sigaction	t_sigaction;
 
-/*
+typedef struct s_prompt
+{
+	char	*current_prompt;
+	bool	is_need_change;
+}	t_prompt;
+
+typedef struct s_info
+{
+	char	*name_shell;
+	char	*pwd;
+}	t_info;
+
+
 typedef	struct s_environment
 {
-
+	t_prompt	prompt;
+	t_info		info;
 }		t_environment;
-*/
+
+//		prompt.c
+void	ft_set_new_prompt(t_prompt	*prompt, t_info	info);
+
+//		init.c
+void	ft_init(t_environment	*env, const char	*name_shell);
+
+//		error.c
+void	ft_error(const char	*name_shell, const char	*err_msg);
+
+//		commands.c
+void	ft_command_cd(t_environment 	*env, const char	*arg);
+char	*ft_get_pwd(void);
+void	ft_commands_pwd(t_environment	*env);
 
 //		utilities_readline.c
 void	ft_readline_insert(const char	*str);
