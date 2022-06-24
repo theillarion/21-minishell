5540e37e39907fa3e7c20f94f92e124682cd013f
Illarion <pro100illarion@gmail.com>
2022-06-24 16:36:02 +0300

    Moved functions

--- srcs/utilities_vector.c
+++ srcs/utilities_vector.c
@@ -1,32 +1,5 @@
 #include "minishell.h"
 
-static bool	ft_check_var_name(const char	*str)
-{
-	while (str && *str)
-	{
-		if (ft_tolower(*str) == *str && *str != '_')
-			return (false);
-		++str;
-	}
-	return (true);
-}
-
-static bool	ft_convert_to_struct(t_variable_env	*dst, const char	*src)
-{
-	char	*ptr;
-	size_t	length;
-
-	length = ft_strlen(src);
-	ptr = ft_strnstr(src, "=", length);
-	if (ptr == NULL || ptr == src)
-		return (false);
-	dst->name = ft_substr(src, 0, (size_t)((ptr - src) / sizeof(*src)));
-	dst->value = ft_substr(src, (size_t)(ptr - src + 1), length - (size_t)(ptr - src + 1));
-	if (dst->name == NULL || ft_check_var_name(dst->name) == false || dst->value == NULL)
-		return (false);
-	return (true);
-}
-
 void	ft_push(t_vector	*vector, const char	*string_var)
 {
 	t_variable_env	var;
