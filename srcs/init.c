512692e0ae8827d011d0c2653e04d3d6263180af
Illarion <pro100illarion@gmail.com>
2022-06-24 14:18:33 +0300

    Added initial and fill vector

--- srcs/init.c
+++ srcs/init.c
@@ -1,10 +1,31 @@
 #include "minishell.h"
 
-void	ft_init(t_environment	*env, const char	*name_shell)
+void	ft_fill_vector(t_vector	*vector, const char	**envp)
+{
+	size_t	i;
+
+	if (vector == NULL || envp == NULL)
+		return;
+	i = 0;
+	while (envp[i] != NULL)
+		++i;
+	ft_reserve(vector, i + 10);
+	i = 0;
+	while (envp[i] != NULL)
+	{
+		ft_push(vector, envp[i]);
+		++i;
+	}
+}
+
+void	ft_init(t_environment	*env, const char	**envp,
+				const char	*name_shell)
 {
 	if (env == NULL)
 		return;
 	ft_initial_action(&env->action);
+	ft_init_vector(&env->variables_env, sizeof(t_variable_env));
+	ft_fill_vector(&env->variables_env, envp);
 	env->info.pwd = ft_get_pwd();
 	env->info.name_shell = ft_strdup(name_shell);
 	env->prompt.current_prompt = NULL;
