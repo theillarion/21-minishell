82f4b117fe08b2d7faaf3a2c1b09a98aed6e3b95
Illarion <pro100illarion@gmail.com>
2022-06-24 14:11:41 +0300

    Added new commands: env and unset

--- srcs/commands.c
+++ srcs/commands.c
@@ -2,7 +2,7 @@
 
 void	ft_command_cd(t_environment 	*env, const char	*arg)
 {
-	if (chdir(arg) == -1)
+	if (env == NULL || chdir(arg) == -1)
 		ft_error(env->info.name_shell, "cd");
 	else
 	{
@@ -28,8 +28,38 @@ char	*ft_get_pwd()
 
 void	ft_command_pwd(t_environment	*env)
 {
-	if (env->info.pwd == NULL)
+	if (env == NULL || env->info.pwd == NULL)
 		ft_error(env->info.name_shell, "pwd");
 	else
 		ft_putendl_fd(env->info.pwd, STDOUT_FILENO);
+}
+
+void	ft_command_env(const t_environment	*env)
+{
+	t_variable_env	var_env;
+	size_t	i;
+
+	if (env == NULL)
+		return;
+	i = 0;
+	while (i < ft_size((const t_vector *)&env->variables_env))
+	{
+		var_env = *(t_variable_env *)ft_get_element(&env->variables_env, i);
+		ft_putstr_fd(var_env.name, STDOUT_FILENO);
+		ft_putchar_fd('=', STDOUT_FILENO);
+		ft_putstr_fd(var_env.value, STDOUT_FILENO);
+		ft_putchar_fd('\n', STDOUT_FILENO);
+		++i;
+	}
+}
+
+void	ft_command_unset(t_environment	*env, const char	*arg)
+{
+	size_t	index;
+
+	if (env == NULL || arg == NULL)
+		return;
+	index = ft_find_by_name(&env->variables_env, arg);
+	if (index < ft_size((const t_vector *)&env->variables_env))
+		ft_erase(&env->variables_env, index);
 }
