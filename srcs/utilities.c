ef1979dfe71beb94c3068e6ce69c9d857f4bc86c
Illarion <pro100illarion@gmail.com>
2022-06-24 15:31:21 +0300

    Added function atoi for convert to int with check error

--- srcs/utilities.c
+++ srcs/utilities.c
@@ -7,3 +7,25 @@ void	ft_smart_free(void	**address)
 	free(*address);
 	*address = NULL;
 }
+
+int	ft_smart_atoi(const char	*src, bool	*is_error)
+{
+	int	sign;
+	int number;
+
+	number = 0;
+	sign = 1;
+	while (*src == 32 || (*src > 8 && *src < 14))
+		++src;
+	if (*src == '-' || *src == '+')
+	{
+		if (*src == '-')
+			sign = -1;
+		++src;
+	}
+	while (ft_isdigit(*src))
+		number = (number * 10) + (*(src++) - '0');
+	if (*src != '\0')
+		*is_error = true;
+	return (number * sign);
+}
