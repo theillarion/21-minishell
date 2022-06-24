d93fb99e36dc3d5bd9d35f0b038f119470f03ed1
Illarion <pro100illarion@gmail.com>
2022-06-24 16:33:50 +0300

    Added new function: ft_command_export. Fixed problems in function ft_command_exit: added check to NULL ptr.

--- srcs/commands_2.c
+++ srcs/commands_2.c
@@ -5,6 +5,8 @@ int	ft_command_exit(t_environment 	*env, const char	*arg)
 	int		code;
 	bool	is_error;
 
+	if (env == NULL || arg == NULL)
+		return (ft_exit(env, COMMON_ERROR, false));
 	is_error = false;
 	code = (unsigned int)ft_smart_atoi(arg, &is_error) % 256;
 	if (is_error)
@@ -13,13 +15,26 @@ int	ft_command_exit(t_environment 	*env, const char	*arg)
 		ft_putstr_fd(": exit: ", STDERR_FILENO);
 		ft_putstr_fd((char *)arg, STDERR_FILENO);
 		ft_putstr_fd(": numeric argument required\n", STDERR_FILENO);
-		ft_exit(env, 2, false);
-		return (2);
+		return (ft_exit(env, 2, false));
 	}
 	else
 	{
 		ft_putendl_fd("exit", STDOUT_FILENO);
-		ft_exit(env, code, false);
-		return (code);
+		return (ft_exit(env, code, false));
 	}
+}
+
+int ft_command_export(t_environment	*env, const char	*arg)
+{
+	t_variable_env	var_env;
+	size_t			index;
+
+	if (env != NULL && ft_convert_to_struct(&var_env, arg) == true)
+	{
+		index = ft_find_by_name(&env->variables_env, var_env.name);
+		if (index < ft_size(&env->variables_env))
+			ft_erase(&env->variables_env, index);
+		ft_push(&env->variables_env, arg);
+	}
+	return (SUCCESS);
 }
