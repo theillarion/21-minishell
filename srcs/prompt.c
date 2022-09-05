73b86b48badb144925f9fe3d8c30690c941ae43a
Illarion <pro100illarion@gmail.com>
2022-09-05 22:11:00 +0300

    Modifided function set_new_prompt: replaces the home directory with a tilde

--- srcs/prompt.c
+++ srcs/prompt.c
@@ -1,21 +1,46 @@
 #include "minishell.h"
 
-void	ft_set_new_prompt(t_prompt	*prompt, t_info	info)
+static char *ft_get_smart_pwd(const t_vector	*variable_env, const char *pwd)
+{
+	char	**ptr;
+	char	*result;
+	size_t	length;
+
+	ptr = ft_get_by_name(variable_env, "HOME")->values;
+	result = NULL;
+	while (ptr && *ptr)
+	{
+		if (ft_strncmp(*ptr, pwd, ft_strlen(*ptr)) == 0)
+			break ;
+		++ptr;
+	}
+	if (*ptr)
+	{
+		length = ft_strlen(pwd) - ft_strlen(*ptr) + 1;
+		result = (char *)malloc((length + 1) * sizeof(*result));
+		result[0] = '~';
+		ft_memcpy((void *)(result + 1), (const void *)(pwd + ft_strlen(*ptr)), length - 1);
+		result[length] = '\0';
+	}
+	return (result);
+}
+
+void	ft_set_new_prompt(const t_vector *variable_env, t_prompt	*prompt, t_info	info)
 {
 	char	*backup;
+	char	*smart_pwd;
 
-	if (prompt == NULL)
-	{
-		//error
-		return ;
-	}
 	backup = prompt->current_prompt;
 	prompt->current_prompt = ft_strjoin(info.name_shell, ":\033[96m");
 	ft_smart_free((void **)&backup);
 	backup = prompt->current_prompt;
-	prompt->current_prompt = ft_strjoin(prompt->current_prompt,
-										info.pwd);
+	smart_pwd = ft_get_smart_pwd(variable_env, info.pwd);
+	if (smart_pwd)
+		prompt->current_prompt = ft_strjoin(prompt->current_prompt, smart_pwd);
+	else
+		prompt->current_prompt = ft_strjoin(prompt->current_prompt, info.pwd);		
 	ft_smart_free((void **)&backup);
+	ft_smart_free((void **)&smart_pwd);
 	backup = prompt->current_prompt;
 	prompt->current_prompt = ft_strjoin(prompt->current_prompt,
 										"\033[0m->");
