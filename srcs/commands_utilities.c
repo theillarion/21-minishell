72fc36a2c3e0292b017db8556308047e75558a5d
Illarion <pro100illarion@gmail.com>
2022-09-05 22:05:01 +0300

    Moved function print_error to print.c

--- srcs/commands_utilities.c
+++ srcs/commands_utilities.c
@@ -13,14 +13,3 @@ char	*ft_get_pwd(void)
 	}
 	return (path);
 }
-
-void ft_print_error(t_environment	*env, const char *command, const char *msg)
-{
-	if (env == NULL || command == NULL || msg == NULL)
-		return ;
-	ft_putstr_fd(env->info.name_shell, STDERR_FILENO);
-	ft_putstr_fd(": ", STDERR_FILENO);
-	ft_putstr_fd((char *)command, STDERR_FILENO);
-	ft_putstr_fd(": ", STDERR_FILENO);
-	ft_putendl_fd((char *)msg, STDERR_FILENO);
-}
