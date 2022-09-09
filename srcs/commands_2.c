6a6227eaf5c3a55ba673fc0e320ada2997ff00da
Illarion <pro100illarion@gmail.com>
2022-09-10 00:06:14 +0300

    Changed all functions: fixed bugs

--- srcs/commands_2.c
+++ srcs/commands_2.c
@@ -7,7 +7,7 @@ int	ft_command_exit(t_environment	*env, const char *const *args)
 
 	ft_putendl_fd("exit", STDOUT_FILENO);
 	if (env == NULL || args == NULL || *args == NULL)
-		return (ft_exit(env, SUCCESS, false));
+		return (ft_exit(env, SUCCESS));
 	if (args != NULL && *args != NULL && *(args + 1) != NULL)
 	{
 		ft_print_error(env, "exit", "too many arguments");
@@ -18,9 +18,9 @@ int	ft_command_exit(t_environment	*env, const char *const *args)
 	if (is_error)
 	{
 		ft_print_error(env, "exit", "numeric argument required");
-		return (ft_exit(env, 2, false));
+		return (ft_exit(env, 2));
 	}
-	return (ft_exit(env, code, false));
+	return (ft_exit(env, code));
 }
 
 int	ft_command_export(t_environment	*env, const char *const *args)
@@ -28,15 +28,19 @@ int	ft_command_export(t_environment	*env, const char *const *args)
 	t_variable_env	var_env;
 	size_t			index;
 
-	if (args == NULL || *args == NULL)
-		ft_command_env(env, args);
-	else if (env != NULL && ft_convert_str_to_struct(&var_env, *args) == true)
+	if (args == NULL)
+		return (ft_command_env(env, args));
+	while (*args)
 	{
-		index = ft_find_by_name(&env->variables_env, var_env.name);
-		if (index < ft_size(&env->variables_env))
-			ft_erase(&env->variables_env, index);
-		ft_push(&env->variables_env, *args);
-		env->is_need_update_envp = true;
+		if (ft_convert_str_to_struct(&var_env, *args))
+		{
+			index = ft_find_by_name(&env->variables_env, var_env.name);
+			if (index < ft_size(&env->variables_env))
+				ft_erase(&env->variables_env, index);
+			ft_push_back(&env->variables_env, (void *)&var_env);
+			env->is_need_update_envp = true;
+		}
+		++args;
 	}
 	return (SUCCESS);
 }
@@ -47,16 +51,16 @@ int	ft_command_echo(t_environment	*env, const char *const *args)
 
 	(void)env;
 	is_need_newline = true;
+	if (args && *args && ft_strlen(*args) == 2 && ft_strncmp(*args, "-n", 2) == 0)
+	{
+		is_need_newline  = false;
+		++args;
+	}
 	while (args && *args)
 	{
-		if (ft_strlen(*args) == 2 && ft_strncmp(*args, "-n", 2) == 0)
-			is_need_newline  = false;
-		else
-		{
-			ft_putstr_fd((char *)*args, STDOUT_FILENO);
-			if (*(args + 1) != NULL)
-				write(1, " ", 1);
-		}
+		ft_putstr_fd((char *)*args, STDOUT_FILENO);
+		if (*(args + 1) != NULL)
+			write(1, " ", 1);
 		++args;
 	}
 	if (is_need_newline)
