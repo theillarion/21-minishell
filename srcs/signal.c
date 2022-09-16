471235b54ca5cf2567f97ab3a93745a8790280f2
glashli <pro100illarion@gmail.com>
2022-09-16 14:55:00 +0300

    Changed signals

--- srcs/signal.c
+++ srcs/signal.c
@@ -1,32 +1,22 @@
 #include "minishell.h"
 
-void    ft_handler(int sig, siginfo_t	*info, void	*ucontext)
+void	ft_handle_signal(int signal)
 {
-    (void)info;
-    (void)ucontext;
-
-	if (sig != SIGINT && sig != SIGQUIT)
-		return;
-	rl_on_new_line();
-	ft_readline_insert("  ");
-/*	rl_on_new_line();
-	rl_replace_line("", 0);
-	//rl_redisplay();*/
-    if (sig == SIGINT)
+    if (signal == SIGINT)
     {
-		write(STDOUT_FILENO, "\n", 1);
-        rl_on_new_line();
+        write(STDOUT_FILENO, "\n", 1);
         rl_replace_line("", 0);
+        rl_on_new_line();
         rl_redisplay();
     }
 }
 
-void	ft_init_action(t_sigaction *action)
+void	ft_handle_signal_child(int signal)
 {
-	ft_memset(&(*action), 0, sizeof(*action));
-	action->sa_flags = SA_SIGINFO;
-	action->sa_sigaction = ft_handler;
-	sigemptyset(&action->sa_mask);
-	sigaddset(&action->sa_mask, SIGQUIT);
-	sigaddset(&action->sa_mask, SIGINT);
+    if (signal == SIGINT)
+        write(STDOUT_FILENO, "\n", 1);
+    else if (signal == SIGQUIT)
+        write(STDOUT_FILENO, "Quit: 3\n", 8);
+    rl_replace_line("", 0);
+    rl_on_new_line();
 }
