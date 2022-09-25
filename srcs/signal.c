12d0e3e82fa3ca8a4043b0a51a8aea6c4c172795
glashli <pro100illarion@gmail.com>
2022-09-25 18:50:50 +0300

    Deleted extra code

--- srcs/signal.c
+++ srcs/signal.c
@@ -22,13 +22,3 @@ void	ft_handle_signal(int signal)
 		rl_redisplay();
 	}
 }
-
-void	ft_handle_signal_child(int signal)
-{
-	if (signal == SIGINT)
-		write(STDOUT_FILENO, "\n", 1);
-	else if (signal == SIGQUIT)
-		write(STDOUT_FILENO, "Quit: 3\n", 8);
-	rl_replace_line("", 0);
-	rl_on_new_line();
-}
